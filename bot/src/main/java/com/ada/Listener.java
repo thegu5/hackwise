e40688485ac56f78941b719e2f3bbf3d2e68434a
Norman He <nohe@cisco.com>
2020-12-05 16:53:54 -0800

    Expanded range of !easteregg options, added a random problem generater, parent resource commands to Listener.java.

--- bot/src/main/java/com/ada/Listener.java
+++ bot/src/main/java/com/ada/Listener.java
@@ -16,18 +16,38 @@ public class Listener implements MessageCreateListener {
 
         if (event.getMessageContent().equalsIgnoreCase("!easteregg")) {
             Random r = new Random();
-            int n = r.nextInt(5);
+            int n = r.nextInt(10);
             switch (n) {
                 case 0:
                     event.getChannel().sendMessage("https://www.khanacademy.org/college-careers-more/personal-finance");
+                    break;
                 case 1:
                     event.getChannel().sendMessage("https://www.khanacademy.org/college-careers-more/learnstorm-growth-mindset-activities-us");
+                    break;
                 case 2:
                     event.getChannel().sendMessage("https://www.youtube.com/watch?v=H6u0VBqNBQ8");
+                    break;
                 case 3:
                     event.getChannel().sendMessage("https://www.khanacademy.org/humanities/high-school-civics");
+                    break;
                 case 4:
                     event.getChannel().sendMessage("https://www.youtube.com/watch?v=BNYJQaZUDrI&list=PL8dPuuaLjXtNgK6MZucdYldNkMybYIHKR");
+                    break;
+                case 5:
+                    event.getChannel().sendMessage("https://www.youtube.com/watch?v=ZQUxL4Jm1Lo");
+                    break;
+                case 6:
+                    event.getChannel().sendMessage("https://www.youtube.com/watch?v=pOLmD_WVY-E");
+                    break;
+                case 7:
+                    event.getChannel().sendMessage("https://www.youtube.com/watch?v=dItUGF8GdTw");
+                    break;
+                case 8:
+                    event.getChannel().sendMessage("https://www.youtube.com/watch?v=deScHJGoVc8");
+                    break;
+                case 9:
+                    event.getChannel().sendMessage("https://hubblesite.org/science/universe");
+                    break;
             }
         }
 
@@ -48,6 +68,41 @@ public class Listener implements MessageCreateListener {
             event.getChannel().sendMessage("https://www.khanacademy.org/science");
 
         }
+        //to generate random math problems
+        if(event.getMessageContent().equalsIgnoreCase("!randomProblem")){
+            Random rand = new Random();
+            int firstterm = rand.nextInt(36)+15;
+            int secondterm = rand.nextInt(36) + 15;
+            int thirdterm = rand.nextInt(51);
+            int s = rand.nextInt(5);
+            String op = "";
+            if(firstterm%2 ==0){
+                op = "-";
+            }
+            switch(s){
+                case 0:
+                    event.getChannel().sendMessage("Solve for x: 0= "+ firstterm+"x^2 + "+secondterm+"x + "+ thirdterm);
+                    break;
+                case 1:
+                    event.getChannel().sendMessage("Draw a graph of the equation: " +op+ firstterm+" + " +thirdterm);
+                    break;
+                case 2:
+                    event.getChannel().sendMessage("solve: " + secondterm/thirdterm);
+                    break;
+                case 3:
+                    event.getChannel().sendMessage("Solve for y: " + firstterm + "y + " +thirdterm + " = "+secondterm + "x + "+s);
+                    break;
+                case 4:
+                    event.getChannel().sendMessage("solve or simplify, if possible: " + firstterm/secondterm);
+                    break;
+            }
+        }
+        //to find parenting resources
+        if(event.getMessageContent().equalsIgnoreCase("!socio-emo")){
+            event.getChannel().sendMessage(" https://bealearninghero.org/parentstrong-2/resources/?tax_language=english&tax_subject=social&search_query&wpas_id=resources-search-parents&wpas_submit=1#results");
+        }else if (event.getMessageContent().equalsIgnoreCase("!childEducation")){
+            event.getChannel().sendMessage("https://www.pbs.org/parents");
+        }
         //command dictionary
         if (event.getMessageContent().equalsIgnoreCase("!help")) {
             event.getChannel().sendMessage("To receive course recommendations:\n" +
@@ -57,18 +112,13 @@ public class Listener implements MessageCreateListener {
                     "!studyScience - to find science courses\n" +
                     "!studyELA - to find English Language Arts courses\n" +
                     "!studyGrammar - to find a grammar course\n" +
-                    "!easteregg - to try a random, interesting course");
+                    "!easteregg - to try a random, interesting course\n" +
+                    "To practice math or challenge yourself with a random problem:\n"+
+                    "!randomProblem - generates a random quadratic equation, graphing, long division, or fraction problem\n"+
+                    "For parenting resources:\n"+
+                    "!social-emo - links you to a page of resources providing tips on how to support children's socio-emotional development\n"+
+                    "!childEducation - links to resources for supplementing the learning of young children at home");
+
         }
     }
-    /*public String nestedResponse(MessageCreateEvent response){
-        if(response.getMessageContent().equalsIgnoreCase("US")){
-
-        }else if (response.getMessageContent().equalsIgnoreCase("World")){
-            event.getChannel().sendMessage("https://www.khanacademy.org/humanities/world-history");
-        }else if (response.getMessageContent().contains("ye")){
-            event.getChannel().sendMessage("https://www.khanacademy.org/science");
-        }else if (response.getMessageContent().contains("no")){
-            event.getChannel().sendMessage("https://www.youtube.com/user/crashcoursekids/playlists");
-        }
-        */
 }
--- bot/src/main/java/com/ada/Main.java
+++ bot/src/main/java/com/ada/Main.java
@@ -1,18 +1,17 @@
 package com.ada;
 
-/**
+/*
  * Created by contemplativemimosa on 12/4/20.
  */
 
 import org.javacord.api.DiscordApi;
 import org.javacord.api.DiscordApiBuilder;
-import org.javacord.api.event.message.MessageCreateEvent;
 
 public class Main {
     public static void main(String[] args){
 
         DiscordApi api = new DiscordApiBuilder()
-                .setToken("Nzg0NTgwODQzMjU5NTU5OTU2.X8rX2Q.ioWbyI-H88dNsEO3vsJFoBsmKx8")
+                .setToken("")
                 .login().join();
 
         api.addListener(new Listener());
